d99a17831893b380a0980963a1fce4c31da3f6e9
NobitaYT032 <77163732+NobitaYT032@users.noreply.github.com>
2021-01-14 10:14:17 +0700

    Add files via upload

---
+++ GL.sh
@@ -0,0 +1,51 @@
+echo "▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀⛔🄽🄾🄱🄸🅃🄰🅈🅃⛔▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"
+echo "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬"
+echo "                  🄵🄾🅁 🄿🅄🄱🄶 🄶🄻🄾🄱🄰🄻"
+echo "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬"
+echo -e "◆▬▬▬▬▬▬▬▬🅃🄴🄻🄴🄶🅁🄰🄼 : @🄱🄰🄽🄶🅂🄰🅃🄺🄰🅄🅄▬▬▬▬▬▬▬▬◆"
+echo "Loading…"
+mkdir /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Config
+cp /data/data/com.termux/files/home/NobitaYT/UserAntiCheat.ini* /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Config
+cp /data/data/com.termux/files/home/NobitaYT/UserEditor.ini* /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Config
+cp /data/data/com.termux/files/home/NobitaYT/UserEngine.ini* /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Config
+cp /data/data/com.termux/files/home/NobitaYT/UserGame.ini* /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Config
+cp /data/data/com.termux/files/home/NobitaYT/UserUAE.ini* /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Config
+sleep 2
+echo "█▒▒▒▒▒▒▒▒▒"
+echo ""
+sleep 2
+echo ""
+echo "███▒▒▒▒▒▒▒10%"
+echo ""
+sleep 2
+echo ""
+echo "█████▒▒▒▒▒30%"
+echo ""
+sleep 2
+echo ""
+echo "███████▒▒▒50%"
+echo ""
+sleep 2
+echo ""
+echo "██████████100%"
+echo""
+echo "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬"
+echo " 🄳🄾🄽'🅃 🄵🄾🅁🄶🄴🅃 🅃🄾 🅂🄴🄽🄳 🄲🄷🄸🄺🄴🄽 🄳🄸🄽🄽🄴🅁 "
+echo "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬"
+echo " 🅈🄾🅄🅃🅄🄱🄴 : 🄽🄾🄱🄸🅃🄰🅈🅃 "
+echo "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬"
+echo " 🄳🄾🄽'🅃 🄵🄾🅁🄶🄴🅃 🅂🅄🄱🅂🄲🅁🄸🄱🄴 "
+echo "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬"
+echo " 🄵🄾🅁 🅅🄸🄿 🄲🄾🄽🅃🄴🄲🅃 : @🄱🄰🄽🄶🅂🄰🅃🄺🄰🅄🅄"
+echo "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬"
+echo "🅃🄴🄻🄴🄶🅁🄰🄼 🄲🄷🄰🄽🄽🄴🄻 : @🄽🄾🄱🄸🅃🄰🅈🅃🄰🄻🄴🅁🅃"
+echo "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬"
+echo ""
+echo ""
+sleep 2
+echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+echo " 🄾🄿🄴🄽 🄿🅄🄱🄶 🄶🄻🄾🄱🄰🄻...."
+echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+sleep 5
+echo ""
+am start -n com.tencent.ig/com.epicgames.ue4.SplashActivity
